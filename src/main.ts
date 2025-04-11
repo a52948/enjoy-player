8e8c723cd847f06402186d4c44f15bbc94043c94
a52948 <529484850@qq.com>
2025-04-11 16:39:03 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,6 @@ import i18n from './i18n'
 import './init/shortcuts'
 import '@/assets/iconfont/iconfont.css'
 import '@/assets/css/main.css'
-import express from 'express';
 import cors from 'cors'; // 确保这一行存在
 
 const app = createApp(App)
